aaeb2115b70783f00caa8891af372a08ff2f9c22
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-08-22 19:12:45 +0200

    The "average" station is just a normal station

---
+++ pyobs_weather/main/stations/average.py
@@ -0,0 +1,41 @@
+import logging
+from datetime import datetime, timedelta
+import pandas as pd
+import pytz
+
+
+log = logging.getLogger(__name__)
+
+
+def update(station_id):
+    from pyobs_weather.main.models import Station, Weather
+    log.info('Updating averages...')
+
+    # get now
+    now = datetime.utcnow().astimezone(pytz.UTC)
+
+    # define fields
+    fields = ['temp', 'humid', 'press', 'winddir', 'windspeed', 'rain', 'skytemp', 'dewpoint', 'particles']
+
+    # loop all stations
+    values = pd.DataFrame({k: [] for k in fields + ['weight']})
+    for station in Station.objects.all():
+        # query all data from the last 5 minutes
+        qs = Weather.objects.filter(station=station, time__gte=now - timedelta(minutes=5))
+        q = qs.values(*fields)
+        df = pd.DataFrame.from_records(q)
+
+        # average
+        mean = df.mean()
+        mean['weight'] = station.weight
+
+        # append to values
+        values = values.append(mean, ignore_index=True)
+
+    # calculate mean of values
+    mean = values.mean().to_dict()
+
+    # write to database
+    v = {k: v for k, v in mean.items() if k in fields}
+    w = Weather(station_id=station_id, time=now, **v)
+    w.save()
--- pyobs_weather/main/tasks.py
+++ pyobs_weather/main/tasks.py
@@ -1,8 +1,5 @@
 import importlib
 import logging
-from datetime import datetime, timedelta
-import pandas as pd
-import pytz
 
 from pyobs_weather.celery import app
 
@@ -13,6 +10,7 @@ log = logging.getLogger(__name__)
 @app.task
 def update_stations(station_code: str):
     from pyobs_weather.main.models import Station
+    log.info('Updating station %s...' % station_code)
 
     # get station
     station = Station.objects.get(code=station_code)
@@ -23,40 +21,3 @@ def update_stations(station_code: str):
 
     # run it
     update_func(station.id)
-
-
-@app.task
-def update_averages(*args):
-    from pyobs_weather.main.models import Station, Weather
-
-    # get now
-    now = datetime.utcnow().astimezone(pytz.UTC)
-
-    # define fields
-    fields = ['temp', 'humid', 'press', 'winddir', 'windspeed', 'rain', 'skytemp', 'dewpoint', 'particles']
-
-    # loop all stations
-    values = pd.DataFrame({k: [] for k in fields + ['weight']})
-    for station in Station.objects.all():
-        # query all data from the last 5 minutes
-        qs = Weather.objects.filter(station=station, time__gte=now - timedelta(minutes=5))
-        q = qs.values(*fields)
-        df = pd.DataFrame.from_records(q)
-
-        # average
-        mean = df.mean()
-        mean['weight'] = station.weight
-
-        # append to values
-        values = values.append(mean, ignore_index=True)
-
-    # calculate mean of values
-    mean = values.mean().to_dict()
-
-    # get average station
-    station = Station.objects.get(code='average')
-
-    # write to database
-    v = {k: v for k, v in mean.items() if k in fields}
-    w = Weather(station=station, time=now, **v)
-    w.save()
