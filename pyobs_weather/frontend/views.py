d4b69be029046789572ec4631d640e57cb4388d8
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-08-26 16:43:55 +0200

    added current values

--- pyobs_weather/frontend/views.py
+++ pyobs_weather/frontend/views.py
@@ -1,11 +1,23 @@
 from django.views.generic import TemplateView
 
-#from pyobs_weather.weather.models import Station, Weather
+from pyobs_weather.weather.models import Station, SensorType, Sensor, Value
 
 
 class OverView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, *args, **kwargs):
-        return {}
-        #return {'stations': Station.objects.all(), 'weather': Weather.objects.filter(station_id=3).all()}
+        # get average weather station
+        station = Station.objects.get(code='average')
+
+        # loop all sensor types
+        values = []
+        for sensor_type in SensorType.objects.all():
+            # loop all sensors for this station and of this type
+            for sensor in Sensor.objects.filter(station=station, type=sensor_type):
+                # get latest value
+                values.append(Value.objects.filter(sensor=sensor).order_by('-time').first())
+
+        # return it
+        print(values)
+        return {'current': values}
