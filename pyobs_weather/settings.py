182333ce2487d24aaf40bf88657a72a31831b40b
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-09-02 17:25:15 +0200

    added fonts and moved static dir

--- pyobs_weather/settings.py
+++ pyobs_weather/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # REDIS related settings
 REDIS_HOST = 'redis'
