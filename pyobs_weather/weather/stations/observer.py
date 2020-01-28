184faa21836dc839b59b91b351288df02703c253
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-01-28 21:11:36 +0200

    using latest temp/press/humid for calculating solar elevation

--- pyobs_weather/weather/stations/observer.py
+++ pyobs_weather/weather/stations/observer.py
@@ -6,6 +6,7 @@ from astropy.time import Time
 from django.conf import settings
 
 from .station import WeatherStation
+from ..models import Value, Sensor
 
 log = logging.getLogger(__name__)
 
@@ -14,8 +15,23 @@ class Observer(WeatherStation):
     def create_sensors(self):
         self._add_sensor('sunalt')
 
+    def _get_latest_value(self, code):
+        # get sensor
+        sensor = Sensor.objects.get(station__code='current', type__code=code)
+
+        # get its latest value
+        val = Value.objects.filter(sensor=sensor).latest('time')
+
+        # return its value
+        return val.value
+
     def update(self):
         log.info('Updating observer info %s...' % self._station.code)
+
+        # get latest values for temp, press and humid
+        temp = self._get_latest_value('temp')
+        press = self._get_latest_value('press')
+        humid = self._get_latest_value('humid')
 
         # get location
         location = EarthLocation(lon=settings.OBSERVER_LOCATION['longitude'] * u.deg,
@@ -27,7 +43,8 @@ class Observer(WeatherStation):
         sun = get_sun(time)
 
         # create alt/az frame
-        altaz_frame = AltAz(location=location, obstime=time,)
+        altaz_frame = AltAz(location=location, obstime=time,
+                            pressure=press * u.hPa, temperature=temp * u.deg_C, relative_humidity=humid)
 
         # convert to alt/az
         sun_altaz = sun.transform_to(altaz_frame)
