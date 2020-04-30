61c98d71491959f06edf6a8cba4abb86a5c92dc6
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-04-30 16:29:09 +0200

    fixed static root

--- pyobs_weather/settings.py
+++ pyobs_weather/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 ROOT_URL = '/'
 STATIC_URL = ROOT_URL + 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/static/'
 
 # REDIS related settings
 REDIS_HOST = 'redis'
