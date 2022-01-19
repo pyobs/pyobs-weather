d175bafad3f789027c3029f9c2b70abfd8c54600
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-01-19 11:10:43 +0100

    added docs

--- pyobs_weather/weather/stations/__init__.py
+++ pyobs_weather/weather/stations/__init__.py
@@ -2,5 +2,7 @@ from .average import Average
 from .observer import Observer
 from .current import Current
 from .mcdlocke import McDonaldLocke
+from .mcdtelnet import McDonaldTelnet
+from .mcdlockearchive import McDonaldLockeArchive
 from .monet import Monet
 from .mysql import MySQL
