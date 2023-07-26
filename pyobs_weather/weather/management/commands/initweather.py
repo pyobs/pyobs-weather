fbecd3a8bcda9022bf223759ab2d6824bf702adb
Tim-Oliver Husser <thusser@uni-goettingen.de>
2023-07-26 21:40:11 +0200

    fixed bug

--- pyobs_weather/weather/management/commands/initweather.py
+++ pyobs_weather/weather/management/commands/initweather.py
@@ -1,50 +1,76 @@
 from django.core.management.base import BaseCommand
-from django_celery_beat.schedulers import CrontabSchedule, IntervalSchedule, PeriodicTask
+from django_celery_beat.schedulers import (
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+)
 
 from pyobs_weather.weather.models import Station, Evaluator
 
 
 class Command(BaseCommand):
-    help = 'Init pyobs-weather'
+    help = "Init pyobs-weather"
 
     def handle(self, *args, **options):
         # create average station
-        interval, _ = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.SECONDS)
-        if Station.objects.filter(code='average').count() == 0:
+        interval, _ = IntervalSchedule.objects.get_or_create(
+            every=10, period=IntervalSchedule.SECONDS
+        )
+        if Station.objects.filter(code="average").count() == 0:
             Station.objects.get_or_create(
-                code='average',
-                name='Average values',
-                class_name='pyobs_weather.weather.stations.Average',
-                crontab=interval
+                code="average",
+                name="Average values",
+                class_name="pyobs_weather.weather.stations.Average",
+                interval=interval,
             )
 
         # create observer station
-        interval, _ = IntervalSchedule.objects.get_or_create(every=30, period=IntervalSchedule.SECONDS)
+        interval, _ = IntervalSchedule.objects.get_or_create(
+            every=30, period=IntervalSchedule.SECONDS
+        )
         interval.save()
-        if Station.objects.filter(code='observer').count() == 0:
+        if Station.objects.filter(code="observer").count() == 0:
             Station.objects.get_or_create(
-                code='observer',
-                name='Observer',
-                class_name='pyobs_weather.weather.stations.Observer',
+                code="observer",
+                name="Observer",
+                class_name="pyobs_weather.weather.stations.Observer",
                 interval=interval,
-                history=False
+                history=False,
             )
 
         # create interval for evaluation
-        interval, _ = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.SECONDS)
+        interval, _ = IntervalSchedule.objects.get_or_create(
+            every=10, period=IntervalSchedule.SECONDS
+        )
         PeriodicTask.objects.get_or_create(
             interval=interval,
-            name='Evaluate sensor goodness',
-            task='pyobs_weather.weather.tasks.evaluate'
+            name="Evaluate sensor goodness",
+            task="pyobs_weather.weather.tasks.evaluate",
         )
 
         # add some default evaluators
-        Evaluator.objects.get_or_create(name='humid', class_name='pyobs_weather.weather.evaluators.SchmittTrigger',
-                                        kwargs='{"good": 80, "bad": 85}')
-        Evaluator.objects.get_or_create(name='windspeed', class_name='pyobs_weather.weather.evaluators.SchmittTrigger',
-                                        kwargs='{"good": 35, "bad": 45}')
-        Evaluator.objects.get_or_create(name='rain', class_name='pyobs_weather.weather.evaluators.Boolean',
-                                        kwargs='{"invert": true}')
-        Evaluator.objects.get_or_create(name='valid', class_name='pyobs_weather.weather.evaluators.Valid', kwargs='{}')
-        Evaluator.objects.get_or_create(name='night', class_name='pyobs_weather.weather.evaluators.Switch',
-                                        kwargs='{"threshold": 0}')
+        Evaluator.objects.get_or_create(
+            name="humid",
+            class_name="pyobs_weather.weather.evaluators.SchmittTrigger",
+            kwargs='{"good": 80, "bad": 85}',
+        )
+        Evaluator.objects.get_or_create(
+            name="windspeed",
+            class_name="pyobs_weather.weather.evaluators.SchmittTrigger",
+            kwargs='{"good": 35, "bad": 45}',
+        )
+        Evaluator.objects.get_or_create(
+            name="rain",
+            class_name="pyobs_weather.weather.evaluators.Boolean",
+            kwargs='{"invert": true}',
+        )
+        Evaluator.objects.get_or_create(
+            name="valid",
+            class_name="pyobs_weather.weather.evaluators.Valid",
+            kwargs="{}",
+        )
+        Evaluator.objects.get_or_create(
+            name="night",
+            class_name="pyobs_weather.weather.evaluators.Switch",
+            kwargs='{"threshold": 0}',
+        )
